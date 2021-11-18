9ed709d6babbd3e68d788fbb63c8824d3ae37668
SerhBondarenko <serhii.bondarenk@gmail.com>
2021-11-18 22:50:56 +0200

    8

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,28 +1,22 @@
-
-
-
 const body = document.querySelector('body')
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 let timerId = null;
 
 startBtn.addEventListener("click", () => {
-timerId = setInterval(() => {
-   body.style.backgroundColor = getRandomHexColor()
+    timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor()
 }, 1000);
     
 startBtn.setAttribute('disabled', 'disabled');
 console.log('Кнопка не активна')
 });
-
-
 stopBtn.addEventListener("click", () => {
     startBtn.removeAttribute('disabled');
     console.log('Кнопка активна')
-  clearInterval(timerId);
+clearInterval(timerId);
 });
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
